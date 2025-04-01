f4df40cd2bcbe97246c1d3476aa9e6f68634343a
chootkuri <doyadoya@sookmyung.ac.kr>
2025-04-01 21:02:05 +0900

    로그인 기능 추가

--- week04/accounts/forms.py
+++ week04/accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2','first_name']
+        fields = ['username', 'password1', 'password2', 'first_name']
--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def signup(request):
     if request.method == "POST":
@@ -9,5 +10,17 @@ def signup(request):
             return redirect("login")
     else:
         form = SignupForm()
-        
+
     return render(request, "signup.html", {"form":form})
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password) # 사용자 인증
+        if user is not None:
+            auth_login(request, user)
+            return redirect("main")
+    
+    return render(request, "login.html")
