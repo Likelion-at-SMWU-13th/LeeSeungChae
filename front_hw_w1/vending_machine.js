712b53135786dbdc0471b3237629e4a198ed5f33
chootkuri <doyadoya@sookmyung.ac.kr>
2025-04-09 18:40:38 +0900

    자판기 html, js 파일 생성

---
+++ front_hw_w1/vending_machine.js
@@ -0,0 +1,25 @@
+// 각 음료의 이름과 가격 정보가 포함된 객체 타입의 배열 생성
+const drinks = [
+    {name: "포카리스웨트", price: 1300},
+    {name: "데미소다", price: 2000},
+    {name: "모구모구", price: 3000},
+    {name: "토레타", price: 2500},
+    {name: "제로콜라", price: 1800},
+];
+
+let my_money; // 지갑 금액을 변수로 설정
+
+
+// 음료 랜덤 선택 기능 - 함수로 구현
+function randomSelectDrink() {
+
+}
+
+// 출력 문자열 반환 기능 - 함수로 구현
+function paymentResult() {
+
+}
+
+function buyDrink() {
+    
+}
