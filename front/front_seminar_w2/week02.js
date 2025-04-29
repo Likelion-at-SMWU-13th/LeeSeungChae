16356a12d058739be625e0693fa7762ad84bd8de
chootkuri <doyadoya@sookmyung.ac.kr>
2025-04-29 19:54:23 +0900

    2. DOM 접근 방법 실습

---
+++ front/front_seminar_w2/week02.js
@@ -0,0 +1,27 @@
+// DOM 요소 선택
+const todoForm = document.getElementById("todo=form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+function addTodo(event) {
+    const todoText = todoInput.value.trim()
+
+    if (todoText !== "") {
+        const todoItem = document.createElement("li");
+        todoItem.className = "todo-item";
+
+        const todoTextSpan = document.createElement("span");
+        todoTextSpan.textContent = todoText;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.className = "delete-btn";
+        deleteBtn.textContent = "삭제";
+
+        todoItem.appendChild(todoTextSpan);
+        todoItem.appendChild(deleteBtn);
+        todoList.appendChild(todoItem);
+
+        todoInput.value ="";
+
+    }
+}
