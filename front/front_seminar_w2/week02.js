116a2f915ae764ed332cd0c3cf47dae4121704f4
chootkuri <doyadoya@sookmyung.ac.kr>
2025-04-29 20:29:05 +0900

    3. 이벤트 처리 실습

--- front/front_seminar_w2/week02.js
+++ front/front_seminar_w2/week02.js
@@ -1,10 +1,14 @@
 // DOM 요소 선택
-const todoForm = document.getElementById("todo=form");
+const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 
+//할 일 추가 함수
+
 function addTodo(event) {
-    const todoText = todoInput.value.trim()
+    event.preventDefault(); //폼 제출 방지
+
+    const todoText = todoInput.value.trim();
 
     if (todoText !== "") {
         const todoItem = document.createElement("li");
@@ -24,4 +28,19 @@ function addTodo(event) {
         todoInput.value ="";
 
     }
+    
+    // 할 일 삭제 처리 함수
+    function handleTodoClick(event) {
+        const target = event.target;
+
+        // 삭제 버튼 클릭 시
+        if (target.classList.contains("delete-btn")) {
+            target.parentElement.remove();
+        }
+    }
+    
+    // 할 일 목록에 클릭 이벤트 리스너 추가
+    todoList.addEventListener("click", handleTodoClick);
 }
+
+todoForm.addEventListener("submit", addTodo);
