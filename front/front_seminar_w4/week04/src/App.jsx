d621b266d2d539c54ebd1441b734e25c4516ab01
chootkuri <doyadoya@sookmyung.ac.kr>
2025-05-20 20:41:23 +0900

    컴포넌트 실습 완료

--- front/front_seminar_w4/week04/src/App.jsx
+++ front/front_seminar_w4/week04/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import LionCard1 from "./components/LionCard1";
+import LionCard3 from "./components/LionCard3";
+import LionCard2 from "./components/LionCard2";
+
 function App() {
-  return <></>;
+  const name = "가짜 아기사자";
+
+  return (
+    <>
+      <h1>{name}를 찾아라!</h1>
+      <LionCard1 />
+      <LionCard2 />
+      <LionCard3 />
+    </>
+  );
 }
 
 export default App;
