d201885b7e85b8fbb2515ab2932f233472cef17a
chootkuri <doyadoya@sookmyung.ac.kr>
2025-07-07 18:56:04 +0900

    아이템 상세 페이지 댓글 등록 기능 추가

--- front/front_hw_w6/week06_hw/src/components/ItemDetail.jsx
+++ front/front_hw_w6/week06_hw/src/components/ItemDetail.jsx
@@ -6,12 +6,14 @@ const ItemDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [item, setItem] = useState(null);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8000/my_items/${id}`)
+      .get(`http://localhost:8000/my_items?id=${id}`)
       .then((res) => {
-        setItem(res.data);
+        setItem(res.data[0]);
       })
       .catch((err) => {
         console.error("에러 발생", err);
@@ -22,9 +24,15 @@ const ItemDetail = () => {
     navigate(-1);
   };
 
-  if (!item) {
-    return <div>불러오는중..</div>;
-  }
+  const addComment = () => {
+    console.log("등록 클릭");
+    if (comment !== "") {
+      setComments([...comments, comment]);
+      setComment("");
+    }
+  };
+
+  if (!item) return <div>로딩 중...</div>;
 
   return (
     <div>
@@ -38,11 +46,20 @@ const ItemDetail = () => {
         <p>비하인드:{item.story}</p>
       </div>
       <div>
-        <input type="text" placeholder="승채템 사용 후기를 남겨주세요!"></input>
-        <button>등록</button>
+        <input
+          type="text"
+          placeholder="승채템 사용 후기를 남겨주세요!"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        ></input>
+        <button onClick={addComment}>등록</button>
       </div>
       <div>
         <p>댓글 모아보기</p>
+        {comments.map((cmt, num) => {
+          console.log(comments);
+          return <div key={num}>{cmt}</div>;
+        })}
       </div>
       <div></div>
     </div>
