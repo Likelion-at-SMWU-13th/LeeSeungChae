d64c3a55edd971bef5da4180d2ec00f8b9a4b425
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-08 16:09:43 +0900

    [Feat] App.jsx 에 컴포넌트 추가 및 뉴스 보여주기 함수 구현

--- front/front_hw_w11/front_hw_w11/src/App.jsx
+++ front/front_hw_w11/front_hw_w11/src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useState } from "react";
+import NewsHeader from "./components/NewsHeader";
+import SeeNews from "./components/SeeNews";
+import NewsList from "./components/NewsList";
+import axios from "axios";
+const newsApi = import.meta.env.VITE_NEWS_API_KEY;
 
 function App() {
-  return <></>;
+  const [news, setNews] = useState([]);
+
+  const showNews = () => {
+    axios
+      .get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsApi}`)
+      .then((res) => {
+        console.log(res.data.articles);
+        setNews(res.data.articles);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  return (
+    <>
+      <NewsHeader title="뉴스를 확인하세요!" />
+      <SeeNews showNews={showNews} />
+      <NewsList news={news} />
+    </>
+  );
 }
 
 export default App;
