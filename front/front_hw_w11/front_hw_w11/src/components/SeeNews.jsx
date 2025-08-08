5809dabc5638784699f1f936d9403d8e6c4d4f3a
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-08 20:42:03 +0900

    [Feat] 뉴스 보기 버튼 스타일 지정

--- front/front_hw_w11/front_hw_w11/src/components/SeeNews.jsx
+++ front/front_hw_w11/front_hw_w11/src/components/SeeNews.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const SearchInput = styled.input``;
+const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-const SeeNewsBtn = styled.button``;
+const SeeNewsBtn = styled.button`
+  color: white;
+  background-color: pink;
+  font-size: 20px;
+  font-weight: medium;
+  border-radius: 50px;
+  padding: 15px;
+  border: none;
+`;
 
 function SeeNews({ showNews }) {
   return (
-    <div>
-      <SeeNewsBtn onClick={showNews}>뉴스 보기</SeeNewsBtn>
-    </div>
+    <BtnContainer>
+      <SeeNewsBtn onClick={showNews}>인기 뉴스 보기👀</SeeNewsBtn>
+    </BtnContainer>
   );
 }
 
