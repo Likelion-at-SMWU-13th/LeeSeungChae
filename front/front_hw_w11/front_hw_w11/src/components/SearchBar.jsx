95eb46918bbbf6104bd0c75b57c5915a95037a19
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-09 09:01:58 +0900

    [Feat]  검색바 스타일 지정

--- front/front_hw_w11/front_hw_w11/src/components/SearchBar.jsx
+++ front/front_hw_w11/front_hw_w11/src/components/SearchBar.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 
-function SearchBar({ showSearchNews }) {
+const SearchBarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  padding: 20px;
+`;
+
+const SearchInput = styled.input`
+  color: black;
+  background-color: white;
+  border: 1px solid #e6e6e6;
+  width: 550px;
+  height: 55px;
+  font-size: 20px;
+  border-radius: 10px 0 0 10px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SearchBtn = styled.button`
+  background-color: white;
+  color: blue;
+  border: 1px solid #e6e6e6;
+  font-size: 20px;
+  border-radius: 0 10px 10px 0;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+function SearchBar({ value, handleChange, showSearchNews }) {
   return (
-    <div>
-      <input></input>
-      <button onClick={showSearchNews}>검색</button>
-    </div>
+    <SearchBarContainer>
+      <SearchInput value={value} onChange={handleChange} />
+      <SearchBtn onClick={showSearchNews}>🔍</SearchBtn>
+    </SearchBarContainer>
   );
 }
 
