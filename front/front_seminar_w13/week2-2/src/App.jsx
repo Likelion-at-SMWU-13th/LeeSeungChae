57a0ead8d551d0692bc486aeaea9312eb5681625
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-09 20:04:03 +0900

    [Feat] App.jsx 작성

--- front/front_seminar_w13/week2-2/src/App.jsx
+++ front/front_seminar_w13/week2-2/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TodoItem from "./components/TodoItem";
+import useTodoStore from "./store/store";
+import * as S from "./styles/styled";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newTodo, setNewTodo] = useState("");
+  const todos = useTodoStore((s) => s.todos);
+  const addTodo = useTodoStore((s) => s.addTodo);
+
+  const handleAddTodo = () => {
+    if (!newTodo.trim()) return;
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAddTodo();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <S.GlobalStyle />
+      <S.Page>
+        <S.Card>
+          <S.H1>투두리스트 ^_^</S.H1>
+
+          <form onSubmit={handleSubmit}>
+            <S.Row>
+              <S.TextInput
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="할 일을 추가해봐~"
+              />
+              <S.PrimartButton type="submit" title="추가">
+                할일추가
+              </S.PrimartButton>
+            </S.Row>
+          </form>
+
+          <S.List>
+            {todos.length === 0 ? (
+              <S.Empty> 할 일이 없어요. 빨리 추가해봅시다!!!</S.Empty>
+            ) : (
+              todos.map((todo) => <todoItem key={todo.id} todo={todo} />)
+            )}
+          </S.List>
+        </S.Card>
+      </S.Page>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
