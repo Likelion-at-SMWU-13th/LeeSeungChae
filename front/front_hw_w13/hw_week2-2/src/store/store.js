b7c08109c21f3011c0c1102e91201223030a7725
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-14 12:50:55 +0900

    store 액션 추가

--- front/front_hw_w13/hw_week2-2/src/store/store.js
+++ front/front_hw_w13/hw_week2-2/src/store/store.js
@@ -1,10 +1,37 @@
 import { create } from "zustand";
 
-const useExerciseStore = create(() => ({
-  exercise: [],
-  addExercise: () => {},
-  removeExercise: () => {},
-  chooseSet: () => {},
+const exerciseData = [
+  { id: 1, name: "스쿼트", sets: 1 },
+  { id: 2, name: "데드리프트", sets: 1 },
+  { id: 3, name: "벤치프레스", sets: 1 },
+];
+
+const useExerciseStore = create((set) => ({
+  exercise: [...exerciseData],
+
+  addExercise: (name) =>
+    set((state) => ({
+      exercise: [...state.exercise, { id: Date.now(), name, sets: 1 }],
+    })),
+
+  removeExercise: (id) =>
+    set((state) => ({
+      exercise: state.exercise.filter((exercise) => exercise.id !== id),
+    })),
+
+  increaseSets: (id) =>
+    set((state) => ({
+      exercise: state.exercise.map((exercise) =>
+        exercise.id === id ? { ...exercise, sets: exercise.sets + 1 } : exercise
+      ),
+    })),
+
+  decreaseSets: (id) =>
+    set((state) => ({
+      exercise: state.exercise.map((exercise) =>
+        exercise.id === id ? { ...exercise, sets: exercise.sets - 1 } : exercise
+      ),
+    })),
 }));
 
 export default useExerciseStore;
