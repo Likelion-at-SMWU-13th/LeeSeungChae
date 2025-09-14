bafb3ee626fa516bdfbb54a29bc9e346b9081191
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-14 14:27:50 +0900

    [Feat] middleware 적용

--- front/front_hw_w13/hw_week2-2/src/store/store.js
+++ front/front_hw_w13/hw_week2-2/src/store/store.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 const exerciseData = [
   { id: 1, name: "스쿼트", sets: 1 },
@@ -6,33 +7,39 @@ const exerciseData = [
   { id: 3, name: "벤치프레스", sets: 1 },
 ];
 
-const useExerciseStore = create((set) => ({
-  allExercise: [...exerciseData],
-  myExercise: [],
+const useExerciseStore = create(
+  devtools((set) => ({
+    allExercise: [...exerciseData],
+    myExercise: [],
 
-  addExercise: (exercise) =>
-    set((state) => ({
-      myExercise: [...state.myExercise, { ...exercise, sets: 1 }],
-    })),
+    addExercise: (exercise) =>
+      set((state) => ({
+        myExercise: [...state.myExercise, { ...exercise, sets: 1 }],
+      })),
 
-  removeExercise: (id) =>
-    set((state) => ({
-      myExercise: state.myExercise.filter((exercise) => exercise.id !== id),
-    })),
+    removeExercise: (id) =>
+      set((state) => ({
+        myExercise: state.myExercise.filter((exercise) => exercise.id !== id),
+      })),
 
-  increaseSets: (id) =>
-    set((state) => ({
-      myExercise: state.myExercise.map((exercise) =>
-        exercise.id === id ? { ...exercise, sets: exercise.sets + 1 } : exercise
-      ),
-    })),
+    increaseSets: (id) =>
+      set((state) => ({
+        myExercise: state.myExercise.map((exercise) =>
+          exercise.id === id
+            ? { ...exercise, sets: exercise.sets + 1 }
+            : exercise
+        ),
+      })),
 
-  decreaseSets: (id) =>
-    set((state) => ({
-      myExercise: state.myExercise.map((exercise) =>
-        exercise.id === id ? { ...exercise, sets: exercise.sets - 1 } : exercise
-      ),
-    })),
-}));
+    decreaseSets: (id) =>
+      set((state) => ({
+        myExercise: state.myExercise.map((exercise) =>
+          exercise.id === id
+            ? { ...exercise, sets: exercise.sets - 1 }
+            : exercise
+        ),
+      })),
+  }))
+);
 
 export default useExerciseStore;
