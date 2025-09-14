4ecbdf360b9f950facd21e0aacc4ee912d379bb7
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-14 14:22:45 +0900

    내가 담은 운동 목록 컴포넌트 생성

--- front/front_hw_w13/hw_week2-2/src/components/ExerciseRoutine.jsx
+++ front/front_hw_w13/hw_week2-2/src/components/ExerciseRoutine.jsx
@@ -1,17 +1,22 @@
 import useExerciseStore from "../store/store";
 
-function ExerciseRoutine({ exercise }) {
+function ExerciseRoutine() {
+  const myExercise = useExerciseStore((s) => s.myExercise);
   const removeExercise = useExerciseStore((s) => s.removeExercise);
   const increaseSets = useExerciseStore((s) => s.increaseSets);
   const decreaseSets = useExerciseStore((s) => s.decreaseSets);
 
   return (
     <div>
-      <h3>{exercise.name}</h3>
-      <button onClick={() => increaseSets(exercise.id)}>+</button>
-      <span>{exercise.sets}세트</span>
-      <button onClick={() => decreaseSets(exercise.id)}>-</button>
-      <button onClick={() => removeExercise(exercise.id)}>삭제</button>
+      {myExercise.map((ex) => (
+        <div key={ex.id}>
+          <h3>{ex.name}</h3>
+          <button onClick={() => increaseSets(ex.id)}>+</button>
+          <span>{ex.sets}세트</span>
+          <button onClick={() => decreaseSets(ex.id)}>-</button>
+          <button onClick={() => removeExercise(ex.id)}>삭제</button>
+        </div>
+      ))}
     </div>
   );
 }
