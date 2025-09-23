a621f2de177803bdd4453db0127295a24ab52648
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-23 20:10:38 +0900

    [Feat] isPending을 사용한 로딩 처리 완료

--- front/front_seminar_w15/week155/src/App.jsx
+++ front/front_seminar_w15/week155/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await response.json();
   }
-  const { data: postData } = useQuery({
+  const { data: postData, isPending } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
-  console.log("queryResult", queryResult);
+  if (isPending) return "로딩 중입니다...";
 
   const posts = postData ?? [];
 
