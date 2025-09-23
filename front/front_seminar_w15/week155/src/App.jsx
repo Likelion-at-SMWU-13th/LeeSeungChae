d8f6be48903b6b4e48c8acdceee92ca6d3326ea4
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-23 19:59:14 +0900

    [Feat] QueryClientProvider 설정 완료

--- front/front_seminar_w15/week155/src/App.jsx
+++ front/front_seminar_w15/week155/src/App.jsx
@@ -1,7 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  return <></>;
+  async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return await response.json();
+  }
+  const queryResult = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+  console.log("queryResult", queryResult);
+
+  return <>tanstack query 실습!</>;
 }
 
 export default App;
