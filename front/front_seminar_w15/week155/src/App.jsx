fb6b1d02b53a6df011935aeefce3ca9ebfe7cab5
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-23 20:16:35 +0900

    [Feat] isError을 사용한 에러 처리 완료

--- front/front_seminar_w15/week155/src/App.jsx
+++ front/front_seminar_w15/week155/src/App.jsx
@@ -4,13 +4,19 @@ import "./App.css";
 function App() {
   async function getPosts() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    return await response.json();
+    // return await response.json();
+    throw new Error("error test");
   }
-  const { data: postData, isPending } = useQuery({
+  const {
+    data: postData,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
   if (isPending) return "로딩 중입니다...";
+  if (isError) return "에러가 발생했습니다.";
 
   const posts = postData ?? [];
 
