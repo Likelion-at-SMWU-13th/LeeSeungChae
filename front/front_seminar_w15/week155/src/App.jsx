c32bfe7afa66f741913d5c5c7c83c9ce173ec74f
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-23 20:08:55 +0900

    [Feat] useQuery를 사용한 데이터 조회 완료

--- front/front_seminar_w15/week155/src/App.jsx
+++ front/front_seminar_w15/week155/src/App.jsx
@@ -6,13 +6,23 @@ function App() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await response.json();
   }
-  const queryResult = useQuery({
+  const { data: postData } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
   console.log("queryResult", queryResult);
 
-  return <>tanstack query 실습!</>;
+  const posts = postData ?? [];
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
