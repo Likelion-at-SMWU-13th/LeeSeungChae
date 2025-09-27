59a4ccafb050f9bf3faa154c11c81f463dfe40a4
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-28 07:47:25 +0900

    개인정보 수정 리액트 쿼리 훅 코드 작성

--- front/front_hw_w15/hw_week15/src/apis/blog/queries.js
+++ front/front_hw_w15/hw_week15/src/apis/blog/queries.js
@@ -1,5 +1,5 @@
-import { useMutation } from "@tanstack/react-query";
-import { createUser } from "@/apis/userApi";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { createUser, updateUser } from "@/apis/userApi";
 
 // 1. 회원가입 (POST)
 export const useCreateUser = () => {
@@ -7,6 +7,21 @@ export const useCreateUser = () => {
     mutationFn: ({ username, password }) => createUser({ username, password }),
     onSuccess: () => {
       alert("환영합니다");
+    },
+  });
+};
+
+// 2. 회원정보 수정 (PUT)
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ userId, username }) => {
+      if (!userId) throw new Error("userId가 없습니다!");
+      return updateUser(userId, username);
+    },
+    onSuccess: () => {
+      alert("정보 수정 완료!");
+      queryClient.invalidateQueries({ queryKey: ["myPage"] });
     },
   });
 };
