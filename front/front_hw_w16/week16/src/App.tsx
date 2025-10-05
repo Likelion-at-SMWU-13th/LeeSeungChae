06e5903cf010736e00f65677bd80b55fe9d73f88
chootkuri <doyadoya@sookmyung.ac.kr>
2025-10-05 13:22:57 +0900

    세부정보템 interface 작성

--- front/front_hw_w16/week16/src/App.tsx
+++ front/front_hw_w16/week16/src/App.tsx
@@ -3,4 +3,13 @@ interface Item {
   itemName: string; // 상품명
   price: number; // 가격
   category: string; // 카테고리
+  isPopular: boolean; // 인기 여부
+}
+
+// 세부 정보 interface
+interface ItemDetail {
+  isFavorite: boolean; // 추천 여부
+  boughtAt: string; // 구매 장소
+  boughtOn: number; // 구매 날짜
+  review: string; // 리뷰
 }
