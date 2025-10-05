209bb7b9f1cf62029c9423deaaa8883f07df4da9
chootkuri <doyadoya@sookmyung.ac.kr>
2025-10-05 13:10:42 +0900

    아이템 interface 작성

--- front/front_hw_w16/week16/src/App.tsx
+++ front/front_hw_w16/week16/src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
-function App() {
-  return <div></div>;
+// 아이템 interface
+interface Item {
+  itemName: string; // 상품명
+  price: number; // 가격
+  category: string; // 카테고리
 }
-
-export default App;
