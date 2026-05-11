276016a47d408b23b94f6b5e54519858129f437d
chootkuri <doyadoya@sookmyung.ac.kr>
2026-05-12 01:22:21 +0900

    Date 실습 완료

---
+++ front/FEweek03/js/date.js
@@ -0,0 +1,18 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+console.log(`${year}년 ${month}월 ${day}일`);
+console.log(`${hours}시 ${minutes}분 ${seconds}초`);
+
+const formattedDate = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+
+const formattedHours = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+console.log(`오늘 날짜: ${formattedDate}`);
+console.log(`현재 시간: ${formattedHours}`);
